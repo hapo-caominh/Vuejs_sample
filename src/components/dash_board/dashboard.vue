febd66a996d560896b9a777bc78350bb3c523383
hapoCaominh <minhcc@haposoft.com>
2018-04-11 18:01:26 +0700

    Add and list hero button

---
+++ src/components/dash_board/dashboard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="child1 container">
+    <span>{{ footnote }}</span>
+    <br>
+    <router-link
+      :to="{name: 'root'}"
+      class="back-main">Back
+    </router-link>
+    <header-dash></header-dash>
+  </div>
+</template>
+
+<script>
+import HeaderVue from '@/components/dash_board/content/header.vue'
+
+export default {
+  data () {
+    return {
+      footnote: 'Dash Board interface'
+    }
+  },
+  components: {
+    'header-dash': HeaderVue
+  }
+}
+</script>
